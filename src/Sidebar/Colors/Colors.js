c6be415294d60e61e419b36cd644c6738028f846
Aditya Shenoy K <a.shenoy.kasargod@accenture.com>
2024-05-31 11:32:18 +0530

    new changes

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,7 +1,33 @@
 import "./Colors.css";
 
 const Colors = () => {
-    return <div>Colors</div>;
+    return (
+        <div>
+            <h2 className="sidebar-title colors-title">Colors</h2>
+            <div>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="category-type" />
+                    <span className="checkmark"></span>All
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="category-type" />
+                    <span className="checkmark"></span>Black
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="category-type" />
+                    <span className="checkmark"></span>Blue
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="category-type" />
+                    <span className="checkmark"></span>Red
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" name="category-type" />
+                    <span className="checkmark"></span>Green
+                </label>
+            </div>
+        </div>
+    );
 };
 
 export default Colors;
